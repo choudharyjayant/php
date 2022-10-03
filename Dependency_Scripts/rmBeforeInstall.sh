962204a783b5fba4c474fe7d5dd3a697e9b60345
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-10-03 11:42:55 +0530

    Update rmBeforeInstall.sh

--- Dependency_Scripts/rmBeforeInstall.sh
+++ Dependency_Scripts/rmBeforeInstall.sh
@@ -1,3 +1,4 @@
-#! /bin/bash
-sudo rm -rf  /home/ubuntu/php/*
-echo "php application"
+#!/bin/bash
+cd /home/ubuntu/php/
+for pid in $(lsof -t -i:8000); do                        kill -9 $pid;                done
+sudo rm -rf *
